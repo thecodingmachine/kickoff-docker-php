c51d1186477babf03f749a13cd480843507d6125
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-11-07 11:58:56 +0100

    first commit

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <title>Is my webserver working?</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<p>If you can read this, the web server is working.</p>
+<?php
+    echo '<p>If you can read this, <a href="phpinfo.php">php</a> is working, too.</p>';
+?>
+</body>
+</html>
