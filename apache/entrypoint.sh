dd58629d9551286ece12bc994dde6dc745ac6d66
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-12-07 15:16:05 +0100

    first try on fixing permissions

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-chgrp www-data /var/www/html;
-chgrp -R www-data /var/www/html;
+usermod -u ${USER_ID} www-data;
+chown www-data:www-data /var/www/html;
+chown -R www-data:www-data /var/www/html;
 chmod g+rw -R $(ls /var/www/html | awk '{if($1 != "vendor"){ print $1 }}');
 
 cron
