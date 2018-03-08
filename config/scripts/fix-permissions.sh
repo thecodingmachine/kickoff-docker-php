88621ced9ea35af1cd15de8e1080d77383d27cd6
Chris Bates <christopher.bates1@gmail.com>
2018-03-09 01:06:45 +1030

    Wrap path in quotations to allow for paths with spaces (#55)

--- config/scripts/fix-permissions.sh
+++ config/scripts/fix-permissions.sh
@@ -13,13 +13,13 @@ NEW_UID=$(id -u);
 
 if [ "$1" == "NGINX" ]; then
     echo "Setting UID ($NEW_UID) to \"www-data\" user in $1 container ...";
-    sedi "s/\${UID}/$NEW_UID/g" ${ROOT}/modules/nginx/docker-compose.yml;
+    sedi "s/\${UID}/$NEW_UID/g" "${ROOT}/modules/nginx/docker-compose.yml";
 elif [ "$1" == "PHP-FPM" ]; then
     echo "Setting UID ($NEW_UID) to \"www-data\" user in $1 container ...";
-    sedi "s/\${UID}/$NEW_UID/g" ${ROOT}/modules/php-fpm/docker-compose.yml;
+    sedi "s/\${UID}/$NEW_UID/g" "${ROOT}/modules/php-fpm/docker-compose.yml";
 else
     echo "Setting UID ($NEW_UID) to \"www-data\" user in Docker Sync configuration file ...";
-    sedi "s/\${UID}/$NEW_UID/g" ${ROOT}/docker-sync.yml;
+    sedi "s/\${UID}/$NEW_UID/g" "${ROOT}/docker-sync.yml";
 fi;
 
 exit 0;
